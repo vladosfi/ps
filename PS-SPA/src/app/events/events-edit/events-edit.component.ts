65de4eb8e7f4bd17375def7bc8ae7d8b9602ff94
vladosfi <vladosfi@gmail.com>
2021-04-02 16:39:52 +0300

    Events Editor

--- PS-SPA/src/app/events/events-edit/events-edit.component.ts
+++ PS-SPA/src/app/events/events-edit/events-edit.component.ts
@@ -13,9 +13,21 @@ import jsonDoc from "./doc";
 export class EventsEditComponent implements OnInit, OnDestroy {
   editor: Editor;
   html: '';
+  eventModel: FormGroup;
+
 
   ngOnInit(): void {
     this.editor = new Editor();
+
+    this.eventModel = new FormGroup({
+      name: new FormControl('', [Validators.required(), Validators.minLength(2), Validators.maxLength(50)]),
+      description: new FormControl('', [Validators.required(), Validators.minLength(10), Validators.maxLength(200)]),
+      editorContent: new FormControl(null, [Validators.required(),Validators.minLength(10)]),
+    });
+  }
+
+  addEvent(){
+
   }
 
   // make sure to destory the editor
