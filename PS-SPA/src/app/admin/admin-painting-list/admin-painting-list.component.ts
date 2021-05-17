ee505a0a54647512b09cbe589225d61bb68e9174
vladosfi <vladosfi@gmail.com>
2021-05-17 17:29:51 +0300

    Fixing Admin Module

--- PS-SPA/src/app/admin/admin-painting-list/admin-painting-list.component.ts
+++ PS-SPA/src/app/admin/admin-painting-list/admin-painting-list.component.ts
@@ -1,9 +1,12 @@
-import { Component, forwardRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { PaginatedResult, Pagination } from 'src/app/_interfaces/pagination';
 import { IPainting } from 'src/app/_interfaces/painting';
 import { PaintingService } from 'src/app/_services/painting.service';
 import { ToastService } from 'src/app/_services/toast.service';
+import { AdminService } from '../admin.service';
+import { debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-painting-list',
@@ -11,26 +14,40 @@ import { ToastService } from 'src/app/_services/toast.service';
   styleUrls: ['./admin-painting-list.component.css']
 })
 export class AdminPaintingListComponent implements OnInit {
+  // @ViewChild('filter', { static: true }) searchInput: ElementRef;
   pagination: Pagination;
   paintings: IPainting[];
   paintingParams: any = {};
+  users$: Observable<any[]>;
 
 
   constructor(private paintingService: PaintingService,
     private toast: ToastService,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private adminService: AdminService) {
 
     document.body.style.backgroundColor = "#A9D3E9";
 
     this.route.data.subscribe(data => {
-      console.log(data);
-
       this.paintings = data['paintings'].result;
       this.pagination = data['paintings'].pagination;
     });
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // this.users$ = fromEvent<KeyboardEvent>(this.searchInput.nativeElement, 'keyup').pipe(
+    //   map(e => (e.target as HTMLInputElement).value),
+    //   startWith(''),
+    //   debounceTime(300),
+    //   distinctUntilChanged(),
+    //   switchMap((inputValue: string) => {
+    //     if (inputValue) {
+    //       console.log(this.searchInput);
+    //       return this.adminService.searchPainting(`?name_like=${inputValue}`)
+    //     }
+    //   })
+    // );
+  }
 
   pageChanged(event: any): void {
     this.pagination.currentPage = event.page;
@@ -55,7 +72,7 @@ export class AdminPaintingListComponent implements OnInit {
     document.body.style.backgroundColor = "#ffffff";
   }
 
-  search(){
+  search() {
     //this.paintingService.getPaintings(this.route.snapshot.params.name);
     // this.paintingParams.searchText;
     // console.log(this.paintingParams.searchText);
