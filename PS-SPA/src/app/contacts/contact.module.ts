b7c7b711319170154560c4613fcd00be83d43a3f
vladosfi <vladosfi@gmail.com>
2021-10-19 17:10:02 +0300

    G-recap

--- PS-SPA/src/app/contacts/contact.module.ts
+++ PS-SPA/src/app/contacts/contact.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
@@ -21,6 +21,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
--- PS-SPA/src/app/contacts/contacts/contacts.component.ts
+++ PS-SPA/src/app/contacts/contacts/contacts.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { IContactFormEntry } from 'src/app/_interfaces/contactFormEntry';
 import { CommonService } from 'src/app/_services/common.service';
 import { ToastService } from 'src/app/_services/toast.service';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators,FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts',
@@ -11,16 +11,34 @@ import { NgForm } from '@angular/forms';
 })
 
 export class ContactsComponent implements OnInit {
-  @ViewChild('contctForm') editForm: NgForm;
-  contactFormEntry: IContactFormEntry = {};
-
-  constructor(private commonService: CommonService,private toast: ToastService) { }
+  contactModel: FormGroup;
+  contactFormEntry: IContactFormEntry;
+  fromNameMinLen: number = 2;
+  fromNameMaxLen: number = 60;
+  sendersEmailMinLen: number = 4;
+  sendersEmailMaxLen: number = 60;
+  contentMinLen: number = 10;
+  contentMaxLen: number = 500;
+  recaptchaValueMinLen: number = 10;
+  recaptchaValueMaxLen: number = 500;
   
-  ngOnInit(): void {
+
+  constructor(private commonService: CommonService,private toast: ToastService, fb: FormBuilder) {
+    
+    this.contactModel = new FormGroup({
+      fromName: new FormControl('', [Validators.required, Validators.minLength(this.fromNameMinLen), Validators.maxLength(this.fromNameMaxLen)]),
+      sendersEmail: new FormControl('', [Validators.required,Validators.email, Validators.minLength(this.sendersEmailMinLen), Validators.maxLength(this.sendersEmailMaxLen)]),
+      content: new FormControl('', [Validators.required, Validators.minLength(this.contentMinLen), Validators.maxLength(this.contentMaxLen)]),
+      recaptchaValue: new FormControl('', [Validators.required, Validators.minLength(this.recaptchaValueMinLen), Validators.maxLength(this.recaptchaValueMaxLen)]),
+   });
+  }
+  
+   ngOnInit(): void {
+
   }
   
   onCaptchaResponse(token: any): void  {
-    this.contactFormEntry.recaptchaValue = token;
+    //this.contactFormEntry.recaptchaValue = token;
     // this.commonService.verifyRecaptcha(token).subscribe(
     //   () => {
     //     console.log('Check Complete');
@@ -31,8 +49,8 @@ export class ContactsComponent implements OnInit {
     // );
   }
 
-  sentEmail(contactFormContent: IContactFormEntry){
-    console.log(contactFormContent.content);
+  sentEmail(){
+    console.log('sdfsdfsdfsdf');
   }
 }
 
